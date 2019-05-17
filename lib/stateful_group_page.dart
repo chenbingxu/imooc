45b093fbf283701f3cfe3b1c33d4f576fdffacad
chenbingxu <chenbingxu@phizmore.com>
2019-05-17 11:07:18 +0800

    完成两个页面切换，if三元表达式。

--- lib/stateful_group_page.dart
+++ lib/stateful_group_page.dart
@@ -45,56 +45,58 @@ class _StatefulGroupState extends State<StatefulGroup> {
             ),
           ],
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          alignment: Alignment.center, // 即（0,0）代表正方形的中间
-          child: Column(
-            children: <Widget>[
-              Text(
-                "I am text",
-                style: textStyle,
-              ),
-              Icon(
-                Icons.android,
-                size: 50,
-                color: Colors.red,
-              ),
-              CloseButton(), //关闭按钮
-              BackButton(), //返回按钮
-              Chip(
-                //material.io/design/components/chips.html
-                avatar: Icon(Icons.photo),
-                label: Text('StatelessWidget与基本组件'),
-              ),
-              Divider(
-                height: 10.0, //容器高度，设置不了线的高度
-                indent: 16.0, //左边间距
-                color: Colors.orange,
-              ),
-              //还可以用Container画线
-              Card(
-                // 带有圆角，阴影，边框效果的卡片
-                color: Colors.blue,
-                elevation: 5, //阴影（悬浮高度）
-                margin: EdgeInsets.all(10),
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'I am Card.',
-                    style: textStyle,
-                  ),
+        body: _currentIndex == 0
+            ? Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
                 ),
-              ),
-              AlertDialog(
-                //这个弹窗将直接显示出来，有一个悬浮和圆角的效果
-                title: Text('盘他'),
-                content: Text('你个糟老头子坏得很'),
-              ),
-            ],
-          ),
-        ),
+                alignment: Alignment.center, // 即（0,0）代表正方形的中间
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "I am text",
+                      style: textStyle,
+                    ),
+                    Icon(
+                      Icons.android,
+                      size: 50,
+                      color: Colors.red,
+                    ),
+                    CloseButton(), //关闭按钮
+                    BackButton(), //返回按钮
+                    Chip(
+                      //material.io/design/components/chips.html
+                      avatar: Icon(Icons.photo),
+                      label: Text('StatelessWidget与基本组件'),
+                    ),
+                    Divider(
+                      height: 10.0, //容器高度，设置不了线的高度
+                      indent: 16.0, //左边间距
+                      color: Colors.orange,
+                    ),
+                    //还可以用Container画线
+                    Card(
+                      // 带有圆角，阴影，边框效果的卡片
+                      color: Colors.blue,
+                      elevation: 5, //阴影（悬浮高度）
+                      margin: EdgeInsets.all(10),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'I am Card.',
+                          style: textStyle,
+                        ),
+                      ),
+                    ),
+                    AlertDialog(
+                      //这个弹窗将直接显示出来，有一个悬浮和圆角的效果
+                      title: Text('盘他'),
+                      content: Text('你个糟老头子坏得很'),
+                    ),
+                  ],
+                ),
+              )
+            : Text("列表"),
       ),
     );
   }
