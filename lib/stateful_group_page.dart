8dec373a247a7bfffeebe91909087f3a910777c3
chenbingxu <chenbingxu@phizmore.com>
2019-05-17 11:55:44 +0800

    TextField，Image，PageView（有点像水平轮播图）

--- lib/stateful_group_page.dart
+++ lib/stateful_group_page.dart
@@ -61,45 +61,32 @@ class _StatefulGroupState extends State<StatefulGroup> {
                       alignment: Alignment.center, // 即（0,0）代表正方形的中间
                       child: Column(
                         children: <Widget>[
-                          Text(
-                            "I am text",
-                            style: textStyle,
+                          // 按住shift拉选清空
+                          Image.network(
+                            'https://flutter.dev/images/catalog-widget-placeholder.png',
+                            width: 100.0,
+                            height: 100.0,
                           ),
-                          Icon(
-                            Icons.android,
-                            size: 50,
-                            color: Colors.red,
-                          ),
-                          CloseButton(), //关闭按钮
-                          BackButton(), //返回按钮
-                          Chip(
-                            //material.io/design/components/chips.html
-                            avatar: Icon(Icons.photo),
-                            label: Text('StatelessWidget与基本组件'),
-                          ),
-                          Divider(
-                            height: 10.0, //容器高度，设置不了线的高度
-                            indent: 16.0, //左边间距
-                            color: Colors.orange,
-                          ),
-                          //还可以用Container画线
-                          Card(
-                            // 带有圆角，阴影，边框效果的卡片
-                            color: Colors.blue,
-                            elevation: 5, //阴影（悬浮高度）
-                            margin: EdgeInsets.all(10),
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: Text(
-                                'I am Card.',
-                                style: textStyle,
-                              ),
+                          TextField(
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                              hintText: '请输入',
+                              hintStyle: TextStyle(fontSize: 15),
+                              //其他属性自己联系，今后搜索模块有深度封装
                             ),
                           ),
-                          AlertDialog(
-                            //这个弹窗将直接显示出来，有一个悬浮和圆角的效果
-                            title: Text('盘他'),
-                            content: Text('你个糟老头子坏得很'),
+                          Container(  //PageView，有点像Banner轮播。
+                            height: 100,
+                            margin: EdgeInsets.only(top: 50),
+                            decoration:
+                                BoxDecoration(color: Colors.lightBlueAccent),
+                            child: PageView(
+                              children: <Widget>[
+                                _item('Page1', Colors.deepPurple),
+                                _item('Page2', Colors.green),
+                                _item('Page3', Colors.red),
+                              ],
+                            ),
                           ),
                         ],
                       ),
@@ -120,5 +107,16 @@ class _StatefulGroupState extends State<StatefulGroup> {
     // return语句，只有await后语句执行完毕后才return一个拿着null的Fature。
     // 可以看到刷新动画持续5秒才结束。化作json读取，这个时长根据真实的await决定。
     return null;
+  }
+
+  _item(String title, MaterialColor color) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(color: color),
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 22, color: Colors.white),
+      ),
+    );
   }
 }
