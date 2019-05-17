ca6a6112ffbf43a4bf5c4e1f012d7929fe8efe9d
chenbingxu <chenbingxu@phizmore.com>
2019-05-17 19:20:13 +0800

    打开手机浏览器，手机地图。

--- lib/launch_page.dart
+++ lib/launch_page.dart
@@ -25,12 +25,15 @@ class _LaunchPageState extends State<LaunchPage> {
                   onPressed: () => _launchURL(),
                   child: Text("打开浏览器"),
                 ),
+                RaisedButton(
+                  onPressed: () => _launchMap(),
+                  child: Text("打开地图"),
+                ),
               ],
             ),
           )),
     );
   }
-
 
   //加载新的插件一定要硬重启
   _launchURL() async {
@@ -41,4 +44,22 @@ class _LaunchPageState extends State<LaunchPage> {
       throw 'Could not launch $url';
     }
   }
+
+  //加载新的插件一定要硬重启
+  _launchMap() async {
+    // Android
+    const url = 'geo:52.32,4.917'; //google地图，App提供者提供的schema，注册在系统的
+    if(await canLaunch(url)){
+      await launch(url);
+    }else{
+      //IOS
+      const url='http://maps.apple.com/?ll=52.32,4.917';
+      if(await canLaunch(url)){
+        await launch(url);
+      }else{
+        throw 'Could not launch $url';
+      }
+    }
+  }
+
 }
